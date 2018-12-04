30fcc384bd9a371d24cc7b27e04343ce869a3f05
Maxim Harder <maxim_harder@jas.de>
2018-12-04 15:33:40 +0100

    Обновление

---
+++ upload/language/MaHarder/de/assets.php
@@ -0,0 +1,41 @@
+<?php
+
+$mh_lang = array(
+
+'functions_31'          =>      "Das Layout-Modul MaHarder Assets ist nicht installiert. Aktuellste Version immer hier: <a href=\"https://github.com/Gokujo/myAssetsDLE\">https://github.com/Gokujo/myAssetsDLE</a>",
+'functions_30'          =>      "Installations- und Aktualisierungsanleitungen, Dokumentation, ...",
+'functions_29'          =>      "Version",
+'functions_28'          =>      "Kontakt",
+'functions_27'          =>      "Änderungen",
+'functions_26'          =>      "Wählen",
+'functions_25'          =>      "Autor",
+'functions_24'          =>      "Einstelungen",
+'functions_23'          =>      "<strong>Finanzierung (Freiwillig)</strong>:",
+'functions_22'          =>      "<strong>Telegram</strong>:",
+'functions_21'          =>      "<strong>Autor</strong>:",
+'functions_20'          =>      "<strong>Copyright</strong>:",
+'functions_19'          =>      "Forderung der Hilfestellung, wenn ein Dritter das Modul modifiziert hatte",
+'functions_18'          =>      "Löschen der Copyrights",
+'functions_17'          =>      "Veröffentlichung des Moduls, falls dieser kostenfrei zur Verfügung steht, ohne Angaben des Autors im Netz",
+'functions_16'          =>      "Veröffentlichungen der Adaptierungen ohne Zustimmung des Autors im Netz",
+'functions_15'          =>      "Forderung des Unmöglichen",
+'functions_14'          =>      "Die Übernahme der Autorenrechte",
+'functions_13'          =>      "<strong>Sie haben KEIN Recht auf:</strong>",
+'functions_12'          =>      "Veröffentlichen des Moduls im Netz, solange dieses kostenfrei auf der Webseite des Autors zur Verfügung steht",
+'functions_11'          =>      "Vorschlagen der Ideen durch die Kontaktwege, die oben angegeben wurden sind",
+'functions_10'          =>      "Adaptierung des Designs",
+'functions_09'          =>      "Adaptierung der Funktionalität",
+'functions_08'          =>      "Erhalt der Hilfestellung durch andere Wegen, die mit dem Autor abgesprochen wurden",
+'functions_07'          =>      "Erhalt der Hilfestellung auf dem <a href=\"\\helplink\" target=\"_blank\" rel=\"noopener\">Forum</a>, auf der <a href=\"\\sitelink\" target=\"_blank\" rel=\"noopener\">Webseite</a> oder im <a href=\"https://t.me/MaHarder\" target=\"_blank\" rel=\"noopener\">Telegram</a> des Autors",
+'functions_06'          =>      "<strong>Sie haben das Recht auf</strong>:",
+'functions_05'          =>      "Falls Sie dieses Modul gekauft haben, so gewährt der Autor bei kleinen Angelegenheiten sofortige Hilfestellung, ansonsten wird diese bei nächstmöglichen Gelegenheit erledigt. Hilfestellung wird sofort entgegengenommen.",
+'functions_04'          =>      "Bei kostenlosen Modulen hilft der Autor <strong>nur</strong> in seiner Freizeit außerhalb von seiner Arbeit. Diese Hilfestellung erfolgt <strong>kostenfrei</strong> und nur in der Kodierung <strong>UTF-8</strong>.",
+'functions_03'          =>      "Dokumentation",
+'functions_02'          =>      "Immer aktuellste Dokumentation: ",
+'functions_01'          =>      "Hilfestellung",
+
+'save'                  =>      "Speichern",
+
+);
+
+?>
